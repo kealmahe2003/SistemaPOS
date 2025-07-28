46dd6b32290926869e8b6cdb5c18c18ede92c3da
Kevin Marin <kealmahe@gmail.com>
2025-07-28 17:16:20 -0500

    Cambio java Swing a JavaFX

--- app/src/main/java/com/cafeteriapos/Main.java
+++ app/src/main/java/com/cafeteriapos/Main.java
@@ -1,23 +1,22 @@
 package com.cafeteriapos;
 
-import com.cafeteriapos.view.LoginView;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        LoginView loginView = new LoginView();
-        Scene scene = new Scene(loginView.getView(), 400, 250);
-        stage.setTitle("Sistema POS - Login");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cafeteriapos/views/LoginView.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Inicio de Sesión - Sistema POS");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
